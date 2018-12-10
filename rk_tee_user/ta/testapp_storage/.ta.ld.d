f010d9db8faea0b9bbfe7c775025ada70b9d90fa
zengwei18488 <wei.zeng@advantech.com.cn>
2018-12-10 11:14:17 +0800

    git added missing files and rm .gitignore

---
+++ rk_tee_user/ta/testapp_storage/.ta.ld.d
@@ -0,0 +1,2 @@
+/home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/ta/testapp_storage/ta.lds: \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/src/ta.ld.S
---
+++ rk_tee_user/ta/testapp_storage/.user_ta_header.o.d
@@ -0,0 +1,16 @@
+/home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/ta/testapp_storage/user_ta_header.o: \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/src/user_ta_header.c \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/include/compiler.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/include/tee_ta_api.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/include/tee_api_defines.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/include/tee_api_types.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/include/stdint.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/lib/gcc/arm-eabi/4.8/include/stdbool.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/lib/gcc/arm-eabi/4.8/include/stddef.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/include/tee_internal_api_extensions.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/include/trace.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/include/trace_levels.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/include/stdio.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/lib/gcc/arm-eabi/4.8/include/stdarg.h \
+ /home/zengwei/work/rk3288-android8.1/android8.1-r46/external/rk_tee_user/export-user_ta/include/user_ta_header.h \
+ include/user_ta_header_defines.h include/ta_testapp_storage.h
